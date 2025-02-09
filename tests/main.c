1e7dcc117e5c8c27082d683ef8db084741fdfcb9
Mateusz Radomski <radomski.main@protonmail.com>
2025-02-09 10:05:13 +0100

    Make sure it's stable

--- tests/main.c
+++ tests/main.c
@@ -94,6 +94,9 @@ UTEST_F_TEARDOWN(SolFmtFixture) {
 
     showDifferences(result, data.output);
     ASSERT_TRUE(stringMatch(result, data.output));
+
+    String result2 = stringTrim(format(&arena, result));
+    ASSERT_TRUE(stringMatch(result2, result));
     arenaDestroy(&arena);
 }
 
