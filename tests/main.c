74165d0642385ff7465a4899d20244c176844113
Mateusz Radomski <radomski.main@protonmail.com>
2025-02-16 17:43:33 +0100

    Const var tests with little expression tests

--- tests/main.c
+++ tests/main.c
@@ -125,5 +125,6 @@ UTEST_F(SolFmtFixture, blockComments) { utest_fixture->path = "tests/blockCommen
 UTEST_F(SolFmtFixture, constructorModifier) { utest_fixture->path = "tests/constructorModifier.sol"; }
 UTEST_F(SolFmtFixture, using) { utest_fixture->path = "tests/using.sol"; }
 UTEST_F(SolFmtFixture, typedefs) { utest_fixture->path = "tests/typedef.sol"; }
+UTEST_F(SolFmtFixture, constVar) { utest_fixture->path = "tests/constVar.sol"; }
 
 UTEST_MAIN();
