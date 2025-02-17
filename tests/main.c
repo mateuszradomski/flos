1d98f892ae78e808d5261d954e6a9e7d084b1dc3
Mateusz Radomski <radomski.main@protonmail.com>
2025-02-17 22:58:54 +0100

    Add ownable test

--- tests/main.c
+++ tests/main.c
@@ -130,5 +130,6 @@ UTEST_F(SolFmtFixture, constVarBreaking) { utest_fixture->path = "tests/constVar
 UTEST_F(SolFmtFixture, arrays) { utest_fixture->path = "tests/arrays.sol"; }
 UTEST_F(SolFmtFixture, inbox) { utest_fixture->path = "tests/inbox.sol"; }
 UTEST_F(SolFmtFixture, libraries) { utest_fixture->path = "tests/libraries.sol"; }
+UTEST_F(SolFmtFixture, ownable) { utest_fixture->path = "tests/ownable.sol"; }
 
 UTEST_MAIN();
