e450bfc7fcbe156ab1aaa7c07ca6df9d4413a7f4
Mateusz Radomski <radomski.main@protonmail.com>
2025-04-01 20:00:12 +0200

    Enable docComments

--- tests/main.c
+++ tests/main.c
@@ -366,5 +366,7 @@ UTEST_F(SolFmtFixture, conditionalOperatorExpression) { utest_fixture->path = "t
 UTEST_F(SolFmtFixture, constructorDefinition) { utest_fixture->path = "tests/foundry/constructorDefinition.sol"; }
 UTEST_F(SolFmtFixture, constructorModifierStyle) { utest_fixture->path = "tests/foundry/constructorModifierStyle.sol"; }
 UTEST_F(SolFmtFixture, doWhileStatement) { utest_fixture->path = "tests/foundry/doWhileStatement.sol"; }
+UTEST_F(SolFmtFixture, docComments) { utest_fixture->path = "tests/foundry/docComments.sol"; }
+
 
 UTEST_MAIN();
