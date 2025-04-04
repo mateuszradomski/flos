3b24d9f3be86ac6e396f116cfdf8b240d9385843
Mateusz Radomski <radomski.main@protonmail.com>
2025-04-04 18:22:36 +0200

    Enable structDefinition

--- tests/foundry/structDefinition.sol
+++ tests/foundry/structDefinition.sol
@@ -8,3 +8,17 @@ struct MyStruct {
     // second
     uint256 field2;
 }
+// Above input, below output
+struct Foo { }
+struct Bar {
+    uint foo;
+    string bar;
+}
+
+struct MyStruct {
+    // first 1
+    // first 2
+    uint256 field1;
+    // second
+    uint256 field2;
+}
--- tests/main.c
+++ tests/main.c
@@ -390,5 +390,6 @@ UTEST_F(SolFmtFixture, operatorExpressions) { utest_fixture->path = "tests/found
 UTEST_F(SolFmtFixture, pragmaDirective) { utest_fixture->path = "tests/foundry/pragmaDirective.sol"; }
 UTEST_F(SolFmtFixture, revertStatement) { utest_fixture->path = "tests/foundry/revertStatement.sol"; }
 UTEST_F(SolFmtFixture, statementBlock) { utest_fixture->path = "tests/foundry/statementBlock.sol"; }
+UTEST_F(SolFmtFixture, structDefinition) { utest_fixture->path = "tests/foundry/structDefinition.sol"; }
 
 UTEST_MAIN();
