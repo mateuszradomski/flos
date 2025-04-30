23d33fa862b65365cabb52cb34323ffc6d47f271
Mateusz Radomski <radomski.main@protonmail.com>
2025-04-30 17:30:30 +0200

    Rename solfmt to flos

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$1" == "" ]; then
-  clang -O2 -o solfmt ./src/linuxMain.c -I./ -DWINDOWS -D_CRT_SECURE_NO_WARNINGS
+  clang -O2 -o flos ./src/linuxMain.c -I./ -DWINDOWS -D_CRT_SECURE_NO_WARNINGS
 fi
 if [ "$1" == "test" ]; then
   clang -g -o test ./tests/main.c -I./src -I./ -DWINDOWS -D_CRT_SECURE_NO_WARNINGS && ./test --enable-mixed-units
