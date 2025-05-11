73cb82c7f82757a01638fc9dce6df0accc027134
Mateusz Radomski <radomski.main@protonmail.com>
2025-05-11 19:02:08 +0200

    Add invalid comment test for later

---
+++ tests/flos/comments.sol
@@ -0,0 +1,19 @@
+contract Comments {
+    //TODO(radomski): Extra newlines that shouldn't be here
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*              SIMPLIFIED FIXED POINT OPERATIONS             */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    function fn() {}
+}
+// Above input, below output
+contract Comments {
+    //TODO(radomski): Extra newlines that shouldn't be here
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+
+    /*              SIMPLIFIED FIXED POINT OPERATIONS             */
+
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    function fn() { }
+}
