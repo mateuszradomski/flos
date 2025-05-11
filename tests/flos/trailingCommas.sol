8b90ce20f68debf6c892c33f6f5ef62acdd0e777
Mateusz Radomski <radomski.main@protonmail.com>
2025-05-11 18:57:41 +0200

    Add test for later

---
+++ tests/flos/trailingCommas.sol
@@ -0,0 +1,13 @@
+contract TrailingCommas {
+    function test() {
+        // TODO(radomski): Crashes
+        // (v0, v1, state.preimageOffset, state.memRoot,,) = sys.handleSysRead(args);
+    }
+}
+// Above input, below output
+contract TrailingCommas {
+    function test() {
+        // TODO(radomski): Crashes
+        // (v0, v1, state.preimageOffset, state.memRoot,,) = sys.handleSysRead(args);
+    }
+}
