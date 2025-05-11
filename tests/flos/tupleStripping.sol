08e2cd2700a832c27fccd63556652b99acf4f9fb
Mateusz Radomski <radomski.main@protonmail.com>
2025-05-11 22:44:27 +0200

    Prepare test for tuple stripping

---
+++ tests/flos/tupleStripping.sol
@@ -0,0 +1,17 @@
+contract TupleStripping {
+    // TODO(radomski): Implement
+    //fallback() external payable {
+    //    address target = addressManager[address(this)].getAddress(
+    //        (implementationName[address(this)])
+    //    );
+    //}
+}
+// Above input, below output
+contract TupleStripping {
+    // TODO(radomski): Implement
+    //fallback() external payable {
+    //    address target = addressManager[address(this)].getAddress(
+    //        (implementationName[address(this)])
+    //    );
+    //}
+}
