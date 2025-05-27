23eb545011a5351b2f758f73ede74aa67d42e2c2
Mateusz Radomski <radomski.main@protonmail.com>
2025-05-27 21:05:34 +0200

    Fix lacking pthread link in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$1" == "" ]; then
-  clang -O2 -o flos ./src/linuxMain.c -I./ -DWINDOWS -D_CRT_SECURE_NO_WARNINGS
+  clang -O2 -o flos ./src/linuxMain.c -I./ -DWINDOWS -D_CRT_SECURE_NO_WARNINGS -pthread
 fi
 if [ "$1" == "test" ]; then
   clang -g -o test ./tests/main.c ./tests/munit.c -I./src -I./ -DWINDOWS -D_CRT_SECURE_NO_WARNINGS -DMUNIT_TEST_NAME_LEN=65 && ./test
