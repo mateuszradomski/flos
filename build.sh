aa9f419a82258e53165224a8fc2ee7b89470ba07
Mateusz Radomski <radomski.main@protonmail.com>
2025-05-31 09:13:59 +0200

    Remove stupid space

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ if [ "$1" == "" ]; then
 fi
 if [ "$1" == "test" ]; then
   clang -g -o test ./tests/main.c ./tests/munit.c -I./src -I./ -DWINDOWS -D_CRT_SECURE_NO_WARNINGS -DMUNIT_TEST_NAME_LEN=65 && ./test
-
 fi
